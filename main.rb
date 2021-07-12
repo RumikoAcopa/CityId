354ec70bd9055a0be9c0894719f8ff0e89368dd4
rumikoacopa <rumikorios@gmail.com>
2021-07-11 21:43:35 -0400

    create var city_info assigned hash w/city names and zip associated with city name

---
+++ main.rb
@@ -0,0 +1,11 @@
+city_info = { 
+  "lilburn" => "30047",
+  "norcross" => "30003",
+  "lawrenceville" => "30044",
+  "dacula" => "30019",
+  "alpharetta" => "30009",
+  "decatur" => "30030",
+  "loganville" => "30052",
+  "marietta" => "30060",
+  "roswell" => "30076",
+ }
